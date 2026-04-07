9be10ad32ae014f4b2e351cd2f2099460207ed23
Pfalconi1529 <patriciofalconi34@gmail.com>
2026-04-07 12:48:40 -0500

    quito un espacio

--- ec2.tf
+++ ec2.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "public_instance" {
   subnet_id              = aws_subnet.public_subnet.id
   key_name               = data.aws_key_pair.key.key_name
   vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
-  user_data              = file("scripts/userdata.sh ")
+  user_data              = file("scripts/userdata.sh")
   tags = {
     "Name" = "${each.value}-${local.sufix}"
   }
